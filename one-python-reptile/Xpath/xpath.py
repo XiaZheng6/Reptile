ddeb4da0dbb57be0d3324393d49d96a690f2a9c8
XiaZheng <hodge_52444@163.com>
2018-01-04 04:29:07 +0800

    Update xpath.py

--- one-python-reptile/Xpath/xpath.py
+++ one-python-reptile/Xpath/xpath.py
@@ -6,4 +6,4 @@ r = requests.get(url).text
 # print r 
 s = etree.HTML(r)
 
-print s.xpath('//*[@id="comments"]/ul/li[1]/div[2]/p/text()')
+print (s.xpath('//*[@id="comments"]/ul/li[1]/div[2]/p/text()'))
