2f97af1afcb4093ebf478e813dba56d08d34cc4b
XiaZheng6 <hodge_52444@163.com>
2018-01-04 22:33:02 +0800

    save file

--- one-python-reptile/Xpath/xpath.py
+++ one-python-reptile/Xpath/xpath.py
@@ -1,9 +1,16 @@
+#coding=utf-8
+
 import requests
 from lxml import etree
 
 url = 'https://book.douban.com/subject/1084336/comments/'
 r = requests.get(url).text
-# print r 
+
 s = etree.HTML(r)
 
-print (s.xpath('//*[@id="comments"]/ul/li[1]/div[2]/p/text()'))
+#print (s.xpath('//*[@id="comments"]/ul/li/div[2]/p/text()'))
+file = s.xpath('//*[@id="comments"]/ul/li/div[2]/p/text()')
+
+with open('pinglun.txt','w',encoding='utf-8') as f:
+    for i in file:
+        f.write(i)
