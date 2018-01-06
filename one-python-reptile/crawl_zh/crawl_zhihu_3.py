124e36a51ae4ed5bb2f51b0109967fb0a9b86a31
XiaZheng <hodge_52444@163.com>
2018-01-06 10:36:02 +0800

    Update crawl_zhihu_3.py

--- one-python-reptile/crawl_zh/crawl_zhihu_3.py
+++ one-python-reptile/crawl_zh/crawl_zhihu_3.py
@@ -9,7 +9,7 @@ headers = {
 	'x-udid':'',
 }
 
-url = 'https://www.zhihu.com/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
+url = 'https://xxxxxxxxxxxxxxxzh/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
 response = requests.get(url,headers=headers).json()['data']
 
 df = pandas.DataFrame.from_dict(response)
