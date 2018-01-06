87863c9b3dc75a651db544ac441799cf6a584c85
XiaZheng <hodge_52444@163.com>
2018-01-06 10:35:08 +0800

    Update crawl_zhihu_1.py

--- one-python-reptile/crawl_zh/crawl_zhihu_1.py
+++ one-python-reptile/crawl_zh/crawl_zhihu_1.py
@@ -2,7 +2,7 @@
 
 import requests
 
-url = 'https://www.zhihu.com/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
+url = 'https://www.xxxxxxxx/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=40&limit=20'
 response = requests.get(url).text
 
 print(response)
