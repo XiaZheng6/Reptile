4920ce49a03e11b360efc880391bdab9c17b189d
XiaZheng <hodge_52444@163.com>
2018-01-06 10:36:47 +0800

    Update crawl_zhihu_4.py

--- one-python-reptile/crawl_zh/crawl_zhihu_4.py
+++ one-python-reptile/crawl_zh/crawl_zhihu_4.py
@@ -12,7 +12,7 @@ headers = {
 user_data = []
 def get_user_data(page):
     for i in range(page):
-        url = 'https://www.zhihu.com/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset={}&limit=20'.format(i*20)
+        url = 'https://zzzzzzzzzzh/api/v4/members/fanclover/followees?include=data%5B*%5D.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F(type%3Dbest_answerer)%5D.topics&offset={}&limit=20'.format(i*20)
         response = requests.get(url,headers=headers).json()['data']
         user_data.extend(response)
         print("正在爬取第%s页" % str(i+1))
